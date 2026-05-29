73a6d5582545cd4b54f79e08a2a7a9cbb3058c13
ofmiho <panada.jantem@e-tech.ac.th>
2026-05-29 14:39:20 +0700

    l2

---
+++ lib/l2.dart
@@ -0,0 +1,23 @@
+void main(){
+  int a = 10 , b = 5 , c;
+  double d;
+
+  print("a = $a");
+  print("b = $b");
+  c = a+b;
+  print("a + b = $c");
+  c = a-b;
+  print("a - b = $c");
+  c = a*b;
+  print("a * b = $c");
+  d = a/b;
+  print("a / b = $d");
+  c = a%b;
+  print("a % b = $c");
+  a += b;
+  print("a += b = $a");
+  a -= b;
+  print("a -= b = $a");
+  a *= b;
+  print("a *= b = $a");
+}
