aaf33b5f54618a5877a212a3c62e60bb83fb4038
ofmiho <panada.jantem@e-tech.ac.th>
2026-05-29 13:47:05 +0700

    first commit

---
+++ lib/test.dart
@@ -0,0 +1,18 @@
+
+void main(){
+
+  String fname = "Panadda";
+  int age = 18;
+  double money = 50.55;
+  bool x = true; //boolean คือ จริงกับเท็จ ระบุในงื่อนไข
+  //var ระบุชนิดข้อมูลอัตโนมัติ
+  //DYNAMIC ตัวแปรอิสระ
+  //LATE กำหนดค่าภายหลัง ใช้กับตัวแปรที่ยังไม่กำหนดค่าในตอนนี้
+  //FANAL กำหนดค่าได้ครั้งเดียว
+  //CONST ค่าคงที่ กำหนคค่าเลยเปลี่ยนค่าไม่ได้
+  //num ระบุแบบกว้างๆได้ทั้ง int & double
+
+
+  print("Hello!  $fname $age $money $x");
+
+}
